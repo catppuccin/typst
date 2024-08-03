641c0908407b5394190dcd7f32aed00039213718
TimeTravelPenguin <35803538+TimeTravelPenguin@users.noreply.github.com>
2024-08-04 00:00:19 +1000

    Corrected output to assets/previews

--- build.sh
+++ build.sh
@@ -3,20 +3,20 @@
 echo "Building Catppuccin pallets for Typst..."
 whiskers catppuccin.tera || exit
 
-mkdir -p assets
+mkdir -p assets/previews
 touch assets/.gitkeep
 
 flavors=("latte" "frappe" "macchiato" "mocha")
 for flavor in "${flavors[@]}"; do
   echo "Compiling demo for '$flavor'..."
-  typst compile --font-path ./font --root . --input flavor="$flavor" ./examples/demo.typ ./assets/"$flavor".png || exit
+  typst compile --font-path ./font --root . --input flavor="$flavor" ./examples/demo.typ ./assets/previews/"$flavor".png || exit
 done
 
 layouts=("composite" "stacked" "grid" "row")
 for layout in "${layouts[@]}"; do
   echo "Creating catwalk with layout '$layout'..."
   catwalk latte.png frappe.png macchiato.png mocha.png \
-    --layout "$layout" --directory ./assets --output catwalk_"$layout".webp || exit
+    --layout "$layout" --directory ./assets/previews --output catwalk_"$layout".webp || exit
 done
 
 echo "Done!"
