38fd833cf60fc41f888be1ca41c07d8cf094e5b7
TimeTravelPenguin <35803538+TimeTravelPenguin@users.noreply.github.com>
2024-08-03 23:58:05 +1000

    Updated build script

--- build.sh
+++ build.sh
@@ -4,16 +4,18 @@ echo "Building Catppuccin pallets for Typst..."
 whiskers catppuccin.tera || exit
 
 mkdir -p assets
+touch assets/.gitkeep
+
 flavors=("latte" "frappe" "macchiato" "mocha")
 for flavor in "${flavors[@]}"; do
   echo "Compiling demo for '$flavor'..."
-  typst compile --font-path ./font --root . --input flavor="$flavor" ./examples/demo.typ ./assets/demo_"$flavor".png || exit
+  typst compile --font-path ./font --root . --input flavor="$flavor" ./examples/demo.typ ./assets/"$flavor".png || exit
 done
 
 layouts=("composite" "stacked" "grid" "row")
 for layout in "${layouts[@]}"; do
   echo "Creating catwalk with layout '$layout'..."
-  catwalk demo_latte.png demo_frappe.png demo_macchiato.png demo_mocha.png \
+  catwalk latte.png frappe.png macchiato.png mocha.png \
     --layout "$layout" --directory ./assets --output catwalk_"$layout".webp || exit
 done
 
